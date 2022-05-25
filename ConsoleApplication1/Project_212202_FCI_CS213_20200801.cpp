4f59fd55c54a0511688f09c00147ba774dbf6a6c
LotusSAMMME <LotusSAMMME@LOTUSSAMMME>
2022-05-25 17:57:32 +0200

    Add project files.

---
+++ ConsoleApplication1/Project_212202_FCI_CS213_20200801.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include "Rational.h"
+
+int main()
+{
+	Rational Rat = Rational(13, 23) + Rational(3, 13);
+	Rational Rat2 = Rational(13, 23) - Rational(3, 13);
+	Rational Rat3 = Rational(13, 23) * Rational(3, 13);
+	Rational Rat4 = Rational(13, 23) / Rational(3, 13);
+	cout << Rational(-6, -9).toString();
+	cout << Rational(2, 2).toString();
+	cout << Rational(2, 1).toString();
+	cout << Rat.toString();
+	cout << Rat2.toString();
+	cout << Rat3.toString();
+	cout << Rat4.toString();
+}
---
+++ ConsoleApplication1/Rational.h
@@ -0,0 +1,136 @@
+
+#if !defined iostream
+#include <iostream>
+#endif
+#if !defined string
+#include <string>
+#endif
+#if !defined cmath
+#include <cmath>
+#endif
+
+class Rational
+{
+private:
+	int numerator;
+	int denominator;
+	int gcd(int n, int d)
+	{
+		if (n == 0)
+		{
+			return abs(d);
+		}
+		else
+			if (d == 0)
+			{
+				return abs(n);
+			}
+			else
+				if (abs(n) == abs(d))
+				{
+					return abs(n);
+				}
+				else
+				{
+					return abs(gcd(d, n % d));
+				}
+
+	}
+
+public:
+	Rational()
+	{
+		numerator = 0;
+		denominator = 1;
+	}
+	Rational(int numera, int denomi)
+	{
+
+		if (denomi != 0)
+		{
+			denominator = denomi;
+		}
+		else
+		{
+			denominator = 1;
+		}
+		numerator = numera;
+		numerator /= gcd(numera, denomi);
+		denominator /= gcd(numera, denomi);
+
+	}
+	int getNumerator()
+	{
+		return numerator;
+	}
+	int getDenominator() { return denominator; }
+	Rational add(Rational secondRational)
+	{
+		int n1 = secondRational.getNumerator();
+		int n2 = getNumerator();
+		int d1 = secondRational.getDenominator();
+		int d2 = getDenominator();
+		int d3_D = d1 * d2;
+		int n3_D = ((n1 * d2) + (n2 * d1));
+		int lCMM = gcd(d3_D, n3_D);
+		d3_D /= lCMM;
+		n3_D /= lCMM;
+		return  Rational(n3_D, d3_D);
+	}
+	Rational subtract(Rational secondRational)
+	{
+		int n1 = secondRational.getNumerator();
+		int n2 = getNumerator();
+		int d1 = secondRational.getDenominator();
+		int d2 = getDenominator();
+		int d3_D = d1 * d2;
+		int n3_D = ((-n1 * d2) + (n2 * d1));
+		int lCMM = gcd(d3_D, n3_D);
+		d3_D /= lCMM;
+		n3_D /= lCMM;
+		return  Rational(n3_D, d3_D);
+	}
+	Rational multiply(Rational secondRational)
+	{
+		int n1 = secondRational.getNumerator();
+		int n2 = getNumerator();
+		int d1 = secondRational.getDenominator();
+		int d2 = getDenominator();
+		int d3_D_b_LCM = d1 * d2 / gcd(d1, d2);
+		int n3_D_b_LCM = ((n2) * (n1)) / gcd(d1, d2);
+		return  Rational(n3_D_b_LCM, d3_D_b_LCM);
+	}
+	Rational divide(Rational secondRational)
+	{
+		int n1 = secondRational.getNumerator();
+		int n2 = getNumerator();
+		int d1 = secondRational.getDenominator();
+		int d2 = getDenominator();
+		int d3_D = (d2 * n1);
+		int n3_D = (d1 * n2);
+		int lCMM = gcd(d3_D, n3_D);
+		d3_D /= lCMM;
+		n3_D /= lCMM;
+		return Rational(n3_D, d3_D);
+	}
+	int compareTo(Rational secondRational)
+	{
+		if (subtract(secondRational).numerator < 0) { return -1; }
+		if (subtract(secondRational).numerator == 0) { return 0; }
+		if (subtract(secondRational).numerator > 0) { return 1; }
+		return 10;
+	}
+	bool equals(Rational secondRational)
+	{
+		return (subtract(secondRational).numerator == 0);
+	}
+	bool operator ==   (Rational right) { return equals(right); }
+	bool operator !=   (Rational right) { return !equals(right); }
+	Rational operator *(Rational right) { return multiply(right); }
+	Rational operator /(Rational right) { return divide(right); }
+	Rational operator +(Rational right) { return add(right); }
+	Rational operator -(Rational right) { return subtract(right); }
+	int intValue() { return int(round(numerator / denominator)); }
+	double doubleValue() { return (numerator / denominator); }
+	string toString() { if (denominator != 1) { return to_string(numerator) + "/" + to_string(denominator) + "\n"; } else return to_string(numerator) + "\n"; }
+};
