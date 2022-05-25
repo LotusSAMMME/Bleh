5a05594d09f566afb700785a5cec1fcba51d758c
Lotus <ElHalawanyYousef@gmail.com>
2022-05-25 18:26:17 +0200

    Edited a line.

--- ConsoleApplication1/Project_212202_FCI_CS213_20200801.cpp
+++ ConsoleApplication1/Project_212202_FCI_CS213_20200801.cpp
@@ -7,7 +7,7 @@ int main()
 	Rational Rat2 = Rational(13, 23) - Rational(3, 13);
 	Rational Rat3 = Rational(13, 23) * Rational(3, 13);
 	Rational Rat4 = Rational(13, 23) / Rational(3, 13);
-	cout << Rational(-6, -9).toString();
+	cout << Rational(6, 9).toString();
 	cout << Rational(2, 2).toString();
 	cout << Rational(2, 1).toString();
 	cout << Rat.toString();
